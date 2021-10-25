e98f4f53591aaf63f39304700e8b6784b9315886
asa_b <asa_b@LAPTOP-1PNC1425.lan>
2021-10-25 18:47:00 +0200

    första testet, ordna de första filerna så som readme.md och .gitignore m.m.

---
+++ readme.md
@@ -0,0 +1,10 @@
+# TicTacToe
+## Gruppuppgift i Java
+
+Åsa:
+AsaBoivie
+
+
+Det här är länken till repository:
+
+https://github.com/AsaBoivie/TicTacToe.git
---
+++ src/com/ya/gruppuppg/TicTacToe.java
@@ -0,0 +1,10 @@
+package com.ya.gruppuppg;
+
+public class TicTacToe {
+
+	public static void main(String[] args) {
+		System.out.println("Testar TicTacToe");
+
+	}
+
+}
