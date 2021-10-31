97e616a9beb0cb6957f323246390ea1f3b5337b8
AsaBoivie <92912552+AsaBoivie@users.noreply.github.com>
2021-10-31 20:51:33 +0100

    Create hasPlayerWon.java
    lägger till hasPlayerWon med en if-sats för att ta reda på om spelare X vunnit, testas med 8 st olika utfall

---
+++ src/com/ya/gruppuppg/hasPlayerWon.java
@@ -0,0 +1,30 @@
+	public static void main(String[] args) {
+
+		// första raden
+		if ((spelPlan[0][0] == 'X' && spelPlan[0][2] == 'X' && spelPlan[0][4] == 'X') ||
+				// andra raden
+				(spelPlan[2][0] == 'X' && spelPlan[2][2] == 'X' && spelPlan[2][4] == 'X') ||
+				// tredje raden
+				(spelPlan[4][0] == 'X' && spelPlan[4][2] == 'X' && spelPlan[4][4] == 'X') ||
+
+				// villkor för första kolumnen
+				(spelPlan[0][0] == 'X' && spelPlan[2][0] == 'X' && spelPlan[4][0] == 'X') ||
+				// andra kolumnen
+				(spelPlan[0][2] == 'X' && spelPlan[2][2] == 'X' && spelPlan[4][2] == 'X') ||
+				// tredje kolumnen
+				(spelPlan[0][4] == 'X' && spelPlan[2][4] == 'X' && spelPlan[4][4] == 'X') ||
+
+				// villkor för första diagonalen
+				(spelPlan[0][0] == 'X' && spelPlan[2][2] == 'X' && spelPlan[4][4] == 'X') ||
+				// andra diagonalen
+				(spelPlan[0][4] == 'X' && spelPlan[2][2] == 'X' && spelPlan[4][0] == 'X'))
+
+		{
+			// Om villkoren ovan uppfylls skrivs detta ut:
+			System.out.println("Spelare X har vunnit tre i rad!");
+		} else {
+			// Annars skrivs detta ut:
+			System.out.println("Spelare X har förlorat");
+		}
+
+	}
