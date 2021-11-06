5529939e6221aeb937b46a4a4f3c02d4514c9bfd
Joan-barcelona <32038566+Joan-barcelona@users.noreply.github.com>
2021-11-06 11:37:00 +0100

    Final version

--- src/com/ya/gruppuppg/Main.java
+++ src/com/ya/gruppuppg/Main.java
@@ -16,8 +16,7 @@ public class Main {
 		boolean taken;
 		int machineTurn;
 		int userInput;
-		
-		
+			
 		// Skriv ut spelplan
 		// rad och kolumn, 2D array
 		
@@ -27,44 +26,12 @@ public class Main {
 		// - + - + -
 		// 7 | 8 | 9		[4][0]	[4][2]	[4][4]
 		
-		char[][] spelPlan = { {'1', '|', '2', '|', '3'}, 
+		char[][] gameBoard = { {'1', '|', '2', '|', '3'}, 
 							  {'-', '+', '-', '+', '-'},
 							  {'4', '|', '5', '|', '6'},
 							  {'-', '+', '-', '+', '-'},
 							  {'7', '|', '8', '|', '9'}};
 		
-		// Skriva ut spelplanen mha 2st for-loopar
-		/*
-		for(char[] rad : spelPlan)	{
-			for(char i : rad)	{
-				System.out.print(i);
-			}
-			System.out.println();
-		} // slut pÃ¥ for
-		*/
-		
-		// Provar att visa spelplanen med 'X' och 'O'.
-		/*
-		System.out.println();
-		
-		spelPlan[0][0] = 'X';
-		spelPlan[0][2] = 'X';
-		spelPlan[0][4] = 'X';
-		spelPlan[2][0] = 'O';
-		spelPlan[2][2] = 'X';
-		spelPlan[2][4] = 'O';
-		spelPlan[4][0] = 'O';
-		spelPlan[4][2] = 'O';
-		spelPlan[4][4] = 'O';
-		
-		for(char[] rad : spelPlan)	{
-			System.out.print("   ");
-			for(char i : rad)	{
-				System.out.print(i);
-			}
-			System.out.println();
-		}
-		*/	
 		
 		turn = 1;  // Det Ã¤r inte mÃ¶jligt mer Ã¤n 5 turer fÃ¶r spelaren pÃ¥ TicTacToe
 		playerWins = false;
@@ -72,17 +39,6 @@ public class Main {
 		Scanner scan = new Scanner(System.in);
 		Random rand = new Random();
 			
-		/*
-		System.out.println();
-		char number = 49; // Number 49 is equal to '1' in ASCII code
-
-		for (int i = 0; i <= 4; i = i + 2) {
-			for (int j = 0; j <= 4; j = j + 2) {
-				spelPlan[i][j] = number;
-				number++;
-			}
-		}
-		*/
 		
 		while((turn <= 5) && (playerWins == false) && (machineWins == false)) {
 					
@@ -91,7 +47,7 @@ public class Main {
 			
 			System.out.println();
 			
-			for (char[] rad : spelPlan) {
+			for (char[] rad : gameBoard) {
 				System.out.print("   ");
 				for (char i : rad) {
 					System.out.print(i);
@@ -100,14 +56,13 @@ public class Main {
 			}		
 			System.out.println();
 			
-			// 2- Player turn
 			
+			// 2- Player turn			
 			// här tar vi in vad spelaren vill göra för drag
 			
 			taken = false;
 			while (taken == false) {
 			
-				//Scanner scan = new Scanner(System.in);
 				System.out.println("I vilken ruta vill du sätta ditt kryss? (1-9)");
 				userInput = scan.nextInt();
 
@@ -116,52 +71,51 @@ public class Main {
 				
 				switch (userInput) {
 					case 1:
-						if (spelPlan[0][0] == '1') {
-							spelPlan[0][0] = 'X';
+						if (gameBoard[0][0] == '1') {
+							gameBoard[0][0] = 'X';
 							taken = true; }
 						break;
 					case 2:
-						if (spelPlan[0][2] == '2') {
-							spelPlan[0][2] = 'X';
+						if (gameBoard[0][2] == '2') {
+							gameBoard[0][2] = 'X';
 							taken = true; }
 						break;
 					case 3:
-						if (spelPlan[0][4] == '3') {
-							spelPlan[0][4] = 'X';
+						if (gameBoard[0][4] == '3') {
+							gameBoard[0][4] = 'X';
 							taken = true; }
 						break;
 					case 4:
-						if (spelPlan[2][0] == '4') {
-							spelPlan[2][0] = 'X';
+						if (gameBoard[2][0] == '4') {
+							gameBoard[2][0] = 'X';
 							taken = true; }
 						break;
 					case 5:
-						if (spelPlan[2][2] == '5') {
-							spelPlan[2][2] = 'X';
+						if (gameBoard[2][2] == '5') {
+							gameBoard[2][2] = 'X';
 							taken = true; }
 						break;
 					case 6:
-						if (spelPlan[2][4] == '6') {
-							spelPlan[2][4] = 'X';
+						if (gameBoard[2][4] == '6') {
+							gameBoard[2][4] = 'X';
 							taken = true; }
 						break;
 					case 7:
-						if (spelPlan[4][0] == '7') {
-							spelPlan[4][0] = 'X';
+						if (gameBoard[4][0] == '7') {
+							gameBoard[4][0] = 'X';
 							taken = true; }
 						break;
 					case 8:
-						if (spelPlan[4][2] == '8') {
-							spelPlan[4][2] = 'X';
+						if (gameBoard[4][2] == '8') {
+							gameBoard[4][2] = 'X';
 							taken = true; }
 						break;
 					case 9:
-						if (spelPlan[4][4] == '9') {
-							spelPlan[4][4] = 'X';
+						if (gameBoard[4][4] == '9') {
+							gameBoard[4][4] = 'X';
 							taken = true; }
 						break;
 					default:
-						//System.out.println("Platsen upptagen, gör ett nytt försök");
 						break;
 				}// slut på switch
 				
@@ -169,175 +123,164 @@ public class Main {
 					System.out.println("Platsen upptagen, gör ett nytt försök");
 				}
 			}// slut på while
-			
-			
-			
+						
 				
 			// 3- Check if player have won
 					
 			// fÃ¶rsta raden
-			if ((spelPlan[0][0] == 'X' && spelPlan[0][2] == 'X' && spelPlan[0][4] == 'X') ||
+			if ((gameBoard[0][0] == 'X' && gameBoard[0][2] == 'X' && gameBoard[0][4] == 'X') ||
 					// andra raden
-					(spelPlan[2][0] == 'X' && spelPlan[2][2] == 'X' && spelPlan[2][4] == 'X') ||
+					(gameBoard[2][0] == 'X' && gameBoard[2][2] == 'X' && gameBoard[2][4] == 'X') ||
 					// tredje raden
-					(spelPlan[4][0] == 'X' && spelPlan[4][2] == 'X' && spelPlan[4][4] == 'X') ||
+					(gameBoard[4][0] == 'X' && gameBoard[4][2] == 'X' && gameBoard[4][4] == 'X') ||
 
 					// villkor fÃ¶r fÃ¶rsta kolumnen
-					(spelPlan[0][0] == 'X' && spelPlan[2][0] == 'X' && spelPlan[4][0] == 'X') ||
+					(gameBoard[0][0] == 'X' && gameBoard[2][0] == 'X' && gameBoard[4][0] == 'X') ||
 					// andra kolumnen
-					(spelPlan[0][2] == 'X' && spelPlan[2][2] == 'X' && spelPlan[4][2] == 'X') ||
+					(gameBoard[0][2] == 'X' && gameBoard[2][2] == 'X' && gameBoard[4][2] == 'X') ||
 					// tredje kolumnen
-					(spelPlan[0][4] == 'X' && spelPlan[2][4] == 'X' && spelPlan[4][4] == 'X') ||
+					(gameBoard[0][4] == 'X' && gameBoard[2][4] == 'X' && gameBoard[4][4] == 'X') ||
 
 					// villkor fÃ¶r fÃ¶rsta diagonalen
-					(spelPlan[0][0] == 'X' && spelPlan[2][2] == 'X' && spelPlan[4][4] == 'X') ||
+					(gameBoard[0][0] == 'X' && gameBoard[2][2] == 'X' && gameBoard[4][4] == 'X') ||
 					// andra diagonalen
-					(spelPlan[0][4] == 'X' && spelPlan[2][2] == 'X' && spelPlan[4][0] == 'X'))
-
+					(gameBoard[0][4] == 'X' && gameBoard[2][2] == 'X' && gameBoard[4][0] == 'X'))
 			{
 				playerWins = true;
-				// Om villkoren ovan uppfylls skrivs detta ut:
-				//System.out.println("Spelare X har vunnit tre i rad!");
-			//} else {
-				// Annars skrivs detta ut:
-				//System.out.println("Spelare X har fÃ¶rlorat");
 			}
 					
 			
 			if((playerWins == false) && (turn <= 4)) {
 				
 				// 4- Machine turn
-				
 				//Slumpmässigt drag
-				
-				// Prova valideling
-				
+							
 				takenCell = true;
 				machineTurn = 0;
 				
 				while( takenCell == true ) {
 					
-					//Random rand = new Random();
 					machineTurn = rand.nextInt(9) + 1;
 					
 					switch(machineTurn) {
 					
-					case 1:
-						if(spelPlan[0][0] == '1') {
-							takenCell = false;
-						}
-						break;
-					case 2:
-						if(spelPlan[0][2] == '2') {
-							takenCell = false;
-						}
-						break;
-					case 3:
-						if(spelPlan[0][4] == '3') {
-							takenCell = false;
-						}
-						break;
-					case 4:
-						if(spelPlan[2][0] == '4') {
-							takenCell = false;
-						}
-						break;
-					case 5:
-						if(spelPlan[2][2] == '5') {
-							takenCell = false;
-						}
-						break;
-					case 6:
-						if(spelPlan[2][4] == '6') {
-							takenCell = false;
-						}
-						break;
-					case 7:
-						if(spelPlan[4][0] == '7') {
-							takenCell = false;
-						}
-						break;
-					case 8:
-						if(spelPlan[4][2] == '8') {
-							takenCell = false;
-						}
-						break;
-					case 9:
-						if(spelPlan[4][4] == '9') {
-							takenCell = false;
-						}
-						break;
+						case 1:
+							if(gameBoard[0][0] == '1') {
+								takenCell = false;
+							}
+							break;
+						case 2:
+							if(gameBoard[0][2] == '2') {
+								takenCell = false;
+							}
+							break;
+						case 3:
+							if(gameBoard[0][4] == '3') {
+								takenCell = false;
+							}
+							break;
+						case 4:
+							if(gameBoard[2][0] == '4') {
+								takenCell = false;
+							}
+							break;
+						case 5:
+							if(gameBoard[2][2] == '5') {
+								takenCell = false;
+							}
+							break;
+						case 6:
+							if(gameBoard[2][4] == '6') {
+								takenCell = false;
+							}
+							break;
+						case 7:
+							if(gameBoard[4][0] == '7') {
+								takenCell = false;
+							}
+							break;
+						case 8:
+							if(gameBoard[4][2] == '8') {
+								takenCell = false;
+							}
+							break;
+						case 9:
+							if(gameBoard[4][4] == '9') {
+								takenCell = false;
+							}
+							break;
 					}					
 				}
 					
 				switch (machineTurn) {
 					case 1:  // Ändrar positionen för 0,0 till O vid rand 1
-						spelPlan[0][0] = 'O';
+						gameBoard[0][0] = 'O';
 						break;
 					case 2:  // Ändrar positionen för 0,2 till O vid rand 2
-						spelPlan[0][2] = 'O';
+						gameBoard[0][2] = 'O';
 						break;
 					case 3:  // Ändrar positionen för 0,4 till O vid rand 3
-						spelPlan[0][4] = 'O';
+						gameBoard[0][4] = 'O';
 						break;
 					case 4:  // Ändrar positionen för 2,0 till O vid rand 4
-						spelPlan[2][0] = 'O';
+						gameBoard[2][0] = 'O';
 						break;
 					case 5:  // Ändrar positionen för 2,2 till O vid rand 5
-						spelPlan[2][2] = 'O';
+						gameBoard[2][2] = 'O';
 						break;
 					case 6:  // Ändrar positionen för 2,4 till O vid rand 6
-						spelPlan[2][4] = 'O';
+						gameBoard[2][4] = 'O';
 						break;
 					case 7:  // Ändrar positionen för 4,0 till O vid rand 7
-						spelPlan[4][0] = 'O';
+						gameBoard[4][0] = 'O';
 						break;
 					case 8:  // Ändrar positionen för 4,2 till O vid rand 8
-						spelPlan[4][2] = 'O';
+						gameBoard[4][2] = 'O';
 						break;
 					case 9:  // Ändrar positionen för 4,4 till O vid rand 9
-						spelPlan[4][4] = 'O';
+						gameBoard[4][4] = 'O';
 						break;
 					default:
 						break;
 				}
-				
-				
+							
 				System.out.println("Datorn valde ruta: " + machineTurn);
 
 										
 				// 5- Check if machine have won
 				// första raden tre i rad
-				if ((spelPlan[0][0] == 'O' && spelPlan[0][2] == 'O' && spelPlan[0][4] == 'O') ||
+				
+				if ((gameBoard[0][0] == 'O' && gameBoard[0][2] == 'O' && gameBoard[0][4] == 'O') ||
 						// andra raden tre i rad
-						(spelPlan[2][0] == 'O' && spelPlan[2][2] == 'O' && spelPlan[2][4] == 'O') ||
+						(gameBoard[2][0] == 'O' && gameBoard[2][2] == 'O' && gameBoard[2][4] == 'O') ||
 						// tredje raden tre i rad
-						(spelPlan[4][0] == 'O' && spelPlan[4][2] == 'O' && spelPlan[4][4] == 'O') ||
+						(gameBoard[4][0] == 'O' && gameBoard[4][2] == 'O' && gameBoard[4][4] == 'O') ||
 	
 						// första kolumnen tre i rad
-						(spelPlan[0][0] == 'O' && spelPlan[2][0] == 'O' && spelPlan[4][0] == 'O') ||
+						(gameBoard[0][0] == 'O' && gameBoard[2][0] == 'O' && gameBoard[4][0] == 'O') ||
 						// andra kolumnen tre i rad
-						(spelPlan[0][2] == 'O' && spelPlan[2][2] == 'O' && spelPlan[4][2] == 'O') ||
+						(gameBoard[0][2] == 'O' && gameBoard[2][2] == 'O' && gameBoard[4][2] == 'O') ||
 						// tredje kolumnen tre i rad
-						(spelPlan[0][4] == 'O' && spelPlan[2][4] == 'O' && spelPlan[4][4] == 'O') ||
+						(gameBoard[0][4] == 'O' && gameBoard[2][4] == 'O' && gameBoard[4][4] == 'O') ||
 	
 						// diagonal uppifrån ned tre i rad
-						(spelPlan[0][0] == 'O' && spelPlan[2][2] == 'O' && spelPlan[4][4] == 'O') ||
+						(gameBoard[0][0] == 'O' && gameBoard[2][2] == 'O' && gameBoard[4][4] == 'O') ||
 						// diagonal nedifrån upp tre i rad
-						(spelPlan[0][4] == 'O' && spelPlan[2][2] == 'O' && spelPlan[4][0] == 'O'))
-	
+						(gameBoard[0][4] == 'O' && gameBoard[2][2] == 'O' && gameBoard[4][0] == 'O'))
 				{
-					// Om villkoren ovan uppfylls skrivs detta ut:
+					// Om villkoren ovan uppfylls gör detta:
 					machineWins = true;
-					//System.out.println("Tyvärr, datorn vann!");
 				} 			
-			}				
+			} // slut på if
+			
 		turn++;	 // NÃ¤sta tur		
-		}
+		} // slut på while
+		
 		
 		// 6- Show winner message
+		
 		System.out.println();
-		for (char[] rad : spelPlan) {
+		for (char[] rad : gameBoard) {
 			System.out.print("   ");
 			for (char i : rad) {
 				System.out.print(i);
@@ -346,15 +289,17 @@ public class Main {
 		}
 		System.out.println();
 		
+		
 		if(playerWins == true) {
-			System.out.println("You win!! Congratulations!");
-		}else if(machineWins) {
-			System.out.println("The machine wins. You lose!");
+			System.out.println("Grattis!! Du vann!");
+		}else if(machineWins == true) {
+			System.out.println("Tyvärr, datorn vann.");
 		}else {
-			System.out.println("Draw. Nobody won.");
-		}
-			 
+			System.out.println("Oavgjort.");
+		}			 
 	}
-
+	
 }
+
+
 
